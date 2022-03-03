2e0b4de26d5fddd15ab0c1474ee4686e3e0ab044
Hakan Okay <hokay18@gmail.com>
2022-03-03 14:21:43 +0300

    New Project Structure

---
+++ apps/test.py
@@ -0,0 +1,44 @@
+# import dash_bootstrap_components as dbc
+# from dash import dcc
+# from dash import html
+# from dash.dependencies import Input, Output
+# from dash import callback_context
+# from func import layout_generator
+#
+# from app import app
+# hs, n_buttons, team_, layout = layout_generator(trigger["prop_id"].split(".")[0].split("-")[-1])
+#
+# app.layout = dbc.Container(
+#     [dbc.DropdownMenu(children=[
+#         dbc.DropdownMenuItem(
+#             "Atlanta Hawks",
+#             href="/atlanta_hawks",
+#             style={"font-size": "12.5px"},
+#             id='Atlanta Hawks'
+#         ),
+#         dbc.DropdownMenuItem(
+#             "Boston Celtics",
+#             href="/boston_celtics",
+#             style={"font-size": "12.5px"},
+#             id='Boston Celtics')
+#     ],
+#         nav=True,
+#         in_navbar=True,
+#         label="Teams",
+#         id="nav_dropdown"
+#     ),
+#         html.Div([], id='my-output')])
+#
+# @app.callback(
+#     Output("my-output", "children"),
+#     [Input("Atlanta Hawks", "n_clicks"),
+#      Input("Boston Celtics", "n_clicks")],
+# )
+# def perfnav(*args):
+#     trigger = callback_context.triggered[0]
+#     layout_generator(trigger["prop_id"].split(".")[0].split("-")[-1])
+#     return
+#
+#
+# if __name__ == '__main__':
+#     app.run_server(debug=True)
