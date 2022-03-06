af5a85ab91a2055e4165b5bdf50c05a3df235051
Hakan Okay <hokay18@gmail.com>
2022-03-06 20:00:13 +0300

    push heroku

--- index.py
+++ index.py
@@ -501,4 +501,5 @@ def render_page_content(pathname):
 
 
 if __name__ == "__main__":
-    serve(server, host="0.0.0.0", port=8050)
+    app.run_server(debug=False)
+    # serve(server, host="0.0.0.0", port=8050)
